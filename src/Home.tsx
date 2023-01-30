6b32576487bad38b1b452ed1d4cd58a9c5c3539d
Vikramaditya <saxenavikramadityasingh@gmail.com>
2023-01-30 18:25:11 +0530

    add validation

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,33 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import "../src/styles.css";
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-  Navigate,
-  useNavigate,
-} from "react-router-dom";
-import Details from "./check";
-
+import { BrowserRouter as Router, Navigate } from "react-router-dom";
+import Check from "./check";
 
 function Home() {
+  const [emailError, setEmailError] = useState("");
+  const [nameError, setNameError] = useState("");
+  const [numError, setNumError] = useState("");
+  // const[disabled,setDisabled] = useState(false);
+
+  const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  const nameRegex = /^[a-zA-Z\s-]+$/;
+  const numRegex = /^[7-9]\d{9}$/;
+  const ifErrors = emailError.length && nameError.length && numError.length;
+
+  function validateName(name: string) {
+    if (nameRegex.test(name)) {
+      setNameError("");
+    } else {
+      setNameError("Enter valid Name!");
+    }
+  }
+
+  function validateEmail(email: string) {
+    if (emailRegex.test(email)) {
+      setEmailError("");
+    } else {
+      setEmailError("Enter valid Email!");
+    }
+  }
+
+  function validateNum(num: string) {
+    if (numRegex.test(num)) {
+      setNumError("");
+    } else {
+      setNumError("Enter valid Number!");
+    }
+  }
+
+  function gotoHandler() {
+    if (!ifErrors) {
+      setGoToNotes(true);
+    } else setGoToNotes(false);
+  }
 
   const [goToNotes, setGoToNotes] = React.useState(false);
   if (goToNotes) {
     return <Navigate to="notes" />;
-
-
-    
-// const expression: RegExp = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
-// const email_id = document.getElementById("email");
-
-// function validateEmail(email_id: string): boolean {
-//   if (expression.test(email_id)) {
-//     console.log("true");
-//     return true;
-//   } else console.log("false");
-//   return false;
-// }
   }
 
   return (
@@ -39,31 +58,71 @@ function Home() {
         <form method="post">
           <label className="details">Enter Full Name</label>
           <br />
-          <input type="text" className="entry-field" />
+          <div className="email-div">
+            <input
+              type="text"
+              placeholder="Full Name"
+              className="email-field"
+              onChange={(e) => validateName(e.target.value)}
+            ></input>{" "}
+            <span
+              style={{
+                fontWeight: "bold",
+                color: "red",
+              }}
+            >
+              {nameError}
+            </span>
+          </div>
           <br />
           <label id="email" className="details">
             Email:
           </label>
           <br />
-          <input type="email" className="entry-field" />
+          <div className="email-div">
+            <input
+              type="text"
+              id="userEmail"
+              placeholder="email Id"
+              className="email-field"
+              onChange={(e) => validateEmail(e.target.value)}
+            ></input>{" "}
+            <span
+              style={{
+                fontWeight: "bold",
+                color: "red",
+              }}
+            >
+              {emailError}
+            </span>
+          </div>
           <br />
-
           <label className="details">Phone Number</label>
           <br />
-          <input type="text" className="entry-field" />
+          <div className="email-div">
+            <input
+              type="text"
+              placeholder="Phone Number"
+              className="email-field"
+              maxLength={10}
+              id="userEmail"
+              onChange={(e) => validateNum(e.target.value)}
+            ></input>{" "}
+            <span
+              style={{
+                fontWeight: "bold",
+                color: "red",
+              }}
+            >
+              {numError}
+            </span>
+          </div>
           <br />
-          <button
-            type="button"
-            onClick={() => {
-              setGoToNotes(true);
-            }}
-            className="button-style"
-          >
+          <button type="button" onClick={gotoHandler} className="button-style">
             <span>Get started</span>
           </button>
         </form>
       </div>
-      {/* <Details /> */}
     </div>
   );
 }
--- src/check.tsx
+++ src/check.tsx
@@ -1,48 +1,93 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
+import validator from "validator";
 
-export default class Details extends Component {
-  state = {
-    email: "",
-    name: "",
-    PhoneNumber: ""
+const Check = () => {
+  const [emailError, setEmailError] = useState("");
+  const validateEmail = (e: React.ChangeEvent<HTMLInputElement>) => {
+    var email = e.target.value;
+
+    if (validator.isEmail(email)) {
+      setEmailError("Valid Email :)");
+    } else {
+      setEmailError("Enter valid Email!");
+    }
   };
 
-  handleChange = (e: { target: { value: any; name: string; }; }) => {
-    const { value, name } = e.target;
-    this.setState({ [name]: value });
-  };
+  return (
+    <div
+      style={{
+        margin: "auto",
+        marginLeft: "300px",
+      }}
+    >
+      <pre>
+        <h2>Validating Email in ReactJS</h2>
+        <span>Enter Email: </span>
+        <input
+          type="text"
+          id="userEmail"
+          onChange={(e) => validateEmail(e)}
+        ></input>{" "}
+        <br />
+        <span
+          style={{
+            fontWeight: "bold",
+            color: "red",
+          }}
+        >
+          {emailError}
+        </span>
+      </pre>
+    </div>
+  );
+};
 
-  render() {
-    return (
-      <div>
-        <form className="col-md-5">
-          <div className="form-group">
-            <label> Email: </label>
-            <input
-              name="email"
-              value={this.state.email}
-              placeholder="email"
-              onChange={this.handleChange}
-            />{" "}
-            <br />
-          </div>
-          <div className="form-group">
-            <label> Name: </label>
-            <input
-              name="name"
-              value={this.state.name}
-              placeholder="name"
-              onChange={this.handleChange}
-            />
-          </div>
-          <button
-            type="button"
-            disabled={!this.state.email || !this.state.name}
-          >
-            Button
-          </button>
-        </form>
-      </div>
-    );
-  }
-}
+export default Check;
+// import React, { Component } from "react";
+
+// export default class Details extends Component {
+//   state = {
+//     email: "",
+//     name: "",
+//     PhoneNumber: ""
+//   };
+
+//   handleChange = (e: { target: { value: any; name: string; }; }) => {
+//     const { value, name } = e.target;
+//     this.setState({ [name]: value });
+//   };
+
+//   render() {
+//     return (
+//       <div>
+//         <form className="col-md-5">
+//           <div className="form-group">
+//             <label> Email: </label>
+//             <input
+//               name="email"
+//               value={this.state.email}
+//               placeholder="email"
+//               onChange={this.handleChange}
+//             />{" "}
+//             <br />
+//           </div>
+//           <div className="form-group">
+//             <label> Name: </label>
+//             <input
+//               name="name"
+//               value={this.state.name}
+//               placeholder="name"
+//               onChange={this.handleChange}
+//             />
+//           </div>
+//           <button
+//             type="button"
+//             disabled={!this.state.email || !this.state.name}
+//           >
+//             Button
+//           </button>
+//         </form>
+//       </div>
+//     );
+//   }
+// }
